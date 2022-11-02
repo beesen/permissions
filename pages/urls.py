bb08a3ccd60d959327aab9336273ef4c13a5731c
beesen <beesen@uvt.nl>
2022-11-02 07:53:25 +0100

    gitignore added

--- pages/urls.py
+++ pages/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'pages'
 urlpatterns = [
     path('', views.home_view, name="home"),
+    path('superuser/', views.superuser_toggle_view, name="superuser-toggle"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -4,3 +4,10 @@ from django.shortcuts import render
 # Create your views here.
 def home_view(request: WSGIRequest):
     return render(request, 'pages/home.html', context={})
+
+def superuser_toggle_view(request: WSGIRequest):
+    # switch superuser
+    user = request.user
+    user.is_superuser = not user.is_superuser
+    user.save()
+    return render(request, 'pages/home.html', context={})
